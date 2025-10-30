a0a3e27cc0b35738bbf31094d7ea6664839fc710
Anan <youandeverythingelse1@gmail.com>
2025-10-30 07:52:20 +0700

    splitting App component splitting App component into multiple smaller components

---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,13 @@
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+function MainMenu() {
+  return (
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+}
+
+export default MainMenu;
