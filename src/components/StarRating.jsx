df4d58eeaffc5aaffa3b8c470b1f2dcd7eaa057f
Anan <youandeverythingelse1@gmail.com>
2025-11-12 05:05:28 +0700

    add rating feature

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+};
+
+function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleClick(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    size: `${size / 1.5}px`,
+  };
+
+  return (
+    <div style={containerStyle} className={className}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            color={color}
+            size={size}
+            onClick={() => handleClick(i + 1)}
+            full={i + 1 <= (tempRating ? tempRating : rating)}
+            onMouseEnter={() => setTempRating(i + 1)}
+            onMouseLeave={() => setTempRating(0)}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating
+          ? tempRating
+          : rating || ""}
+      </p>
+    </div>
+  );
+}
+
+export default StarRating;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// import StarRating from "./components/StarRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    {/* <StarRating maxRating={9} /> */}
   </StrictMode>
 );
